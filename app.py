f19471b62b5dcd728ca421894156418401460234
haideigv <haideiviacheslav@yahoo.com>
2016-06-07 23:43:29 +0300

    rooms

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-from flask.ext.socketio import SocketIO, emit, join_room, leave_room, close_room, disconnect, send
-from flask import logging
-from extension import login_manager, LoginManager, CsrfProtect
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room, close_room, disconnect, send, rooms
 from gevent import monkey
 monkey.patch_all()
 
@@ -9,70 +7,71 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-
+# show game board
 @app.route('/game')
 def index():
     return render_template('index.html')
 
-
+# login func
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    session['game'] = {}
-    session['counter'] = 0
     error = None
     if request.method == 'POST':
         if request.form['username'] not in ['admin1', 'admin2'] or request.form['password'] not in ['admin1', 'admin2']:
             error = 'Invalid Credentials. Please try again.'
         else:
             session['username'] = request.form['username']
-            if not session['game']:
-                session['game']['user1'] = {0: request.form['username']}
-            else:
-                session['game']['user2'] = {1: request.form['username']}
-            print(session['game'])
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
 
+# use the room if the room have less than 1 users
 @socketio.on('join')
-def join(message):
-    join_room(message['room'])
+def on_join(data):
+    username = session['username']
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response', {'data': 'In rooms: ' + ', '.join(request.namespace.rooms), 'count': session['receive_count']})
+    room = data['room']
+    if session['receive_count'] <= 2:
+        join_room(room)
+        emit('my response', {'data': username+' connected. In rooms: ' + ', '.join(rooms()), 'count': session['receive_count']})
+    else:
+        emit('my response', {'data': username+' not connected. In room 2 gamers '})
 
+
+# out from the room
 @socketio.on('leave')
 def on_leave(data):
-    username = data['username']
+    username = session['username']
     room = data['room']
     leave_room(room)
     send(username + ' has left the room.', room=room)
 
 
-
+# function for display each events on the right chat
 @socketio.on('my event', namespace='/test')
 def test_message(message):
     cell_name = session['username']
     emit('my response', {'data': message['data'], 'cell_name': cell_name}, broadcast=True)
 
+
+# function which send username into cells, which was clicked
 @socketio.on('cell event', namespace='/test')
 def test_message(message):
     cell_name = session['username']
-    emit('cell response', {'data': message['data'], 'cell_name': cell_name }, broadcast=True)
+    emit('cell response', {'data': message['data'], 'cell_name': cell_name}, broadcast=True)
 
-@socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
-    emit('login response', {'data': message['data']})
 
+#connect function
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    cell_name = session['username']
-    emit('my connecting', {'data': 'Connected', 'cell_name': cell_name}, broadcast=True)
+    name = session['username']
+    emit('my connecting', {'data': 'Connected', 'name': name}, broadcast=True)
 
+
+# disconnect function
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected!')
-
-
 
 
 if __name__ == '__main__':
