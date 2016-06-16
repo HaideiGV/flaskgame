2679c11349091d3978ba2dc9b64f1f1b7b78fd78
HaideiGV <haideigv@gmail.com>
2016-06-16 12:22:14 +0300

    hot fix

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def test_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
